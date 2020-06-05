bfc2641724ce045f350f0915f63c9a3646619b51
prakhar9998 <prakhar.varshney33@gmail.com>
2020-06-05 12:58:50 +0530

    Add SFML window for rendering

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello World\n";
+    sf::RenderWindow window(sf::VideoMode(800, 800), "3D Software Renderer");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+        
+        window.display();
+    }
+
     return 0;
 }
